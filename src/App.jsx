559d1ae5e600e5cb96775c94839cdf10d043f7d7
Aditya <adityasingh11968@gmail.com>
2025-12-27 21:43:54 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import React from "react";
-import "semantic-ui-css/semantic.min.css";
+// import "semantic-ui-css/semantic.min.css";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
-import Testimonials from "./Components/Testimonials";
 import Stickers from "./Components/Stickers";
 import FAQs from "./Components/FAQs";
 import Footer from "./Components/Footer";
 import "./App.scss";
-import Fotoroma from "./Components/Fotoroma";
+import { Suspense, lazy } from "react";
+import { Loader } from 'semantic-ui-react'
+
+const Fotoroma = lazy(() => import("./Components/Fotoroma"));
+const Testimonials = lazy(() => import("./Components/Testimonials"));
 
 const App = () => {
   return (
     <div className="maincontainer">
       <Header />
       <Navbar />
-      <Fotoroma />
+      <Suspense fallback={<div><Loader active inline='centered' /></div>}>
+        <Fotoroma />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Testimonials />
+      </Suspense>
       <br />
       <br />
       <br />
       <br />
-      <Testimonials />
-       <br />
-      <br />
-      <br />
-      <br />
-      <Stickers/>
-     {/* <FAQs/>
+      <Stickers />
+      {/* <FAQs/>
      <Footer/> */}
     </div>
   );
